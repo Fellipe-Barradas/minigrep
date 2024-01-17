de85e86430dd8225d6daeff4f4c96252b3faba59
Luis Fellipe <luisbezerrabarradas@gmail.com>
2024-01-17 20:41:05 -0300

    Refactor command handling and add minigrep run function

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+use clap::Parser;
+use std::process;
+use commands::CommandsEnum;
+
+mod commands;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    /// Command to run
+    #[command(subcommand)]
+    pub command: Option<CommandsEnum>,
+}
+
+use commands::{count, find_word};
+pub fn run(){
+    let args = Cli::parse();
+
+    match &args.command {
+        Some(CommandsEnum::Count{input, file}) => {
+            count(input, file).unwrap_or_else(|err|{
+                println!("Erro ao tentar abrir o arquivo, {}", err.to_string());
+                process::exit(1)
+            });
+        },
+        Some(CommandsEnum::Find { input, file }) => {
+            find_word(input, file).unwrap_or_else(|err|{
+                println!("Erro ao tentar abrir o arquivo, {}", err.to_string());
+                process::exit(1)
+            });
+        }
+        None => {
+            println!("No command");
+        }
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,5 @@
-use std::process;
-
-use clap::Parser;
-
-mod commands;
-
-use commands::*;
-
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    /// Turn debugging information on
-    #[command(subcommand)]
-    command: Option<CommandsEnum>,
-}
+use minigrep::run;
 
 fn main() {
-   let args = Cli::parse();
-
-   match &args.command {
-        Some(CommandsEnum::Count{input, file}) => {
-            count(input, file).unwrap_or_else(|err|{
-                println!("Erro ao tentar abrir o arquivo, {}", err.to_string());
-                process::exit(1)
-            });
-        },
-        Some(CommandsEnum::Find { input, file }) => {
-            find_word(input, file).unwrap_or_else(|err|{
-                println!("Erro ao tentar abrir o arquivo, {}", err.to_string());
-                process::exit(1)
-            });
-        }
-        None => {
-            println!("No command");
-        }
-
-   } 
-}
-
-#[test]
-fn verify_cli(){
-    use clap::CommandFactory;
-    Cli::command().debug_assert();
+    run();
 }
